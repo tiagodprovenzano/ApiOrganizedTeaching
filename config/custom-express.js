aaa6c0c0543776b6c21bd20a8ee699b345f20614
Tiago Dias <tiagodprovenzano@gmail.com>
2019-06-16 17:50:35 -0300

    maybe now cors available

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,9 +1,11 @@
 const express =  require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 // const sessionAuth = require('./sessionAuth')
 module.exports = () => {
     const app = express()
+        app.use(cors())
         app.use(bodyParser.urlencoded({extended:true}))
         app.use(bodyParser.json())
         app.use(bodyParser.json())
