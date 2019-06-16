b2f6fa7d19afdb99bcfe697551ef1e4c5fd18856
Tiago Dias <tiagodprovenzano@gmail.com>
2019-06-16 17:52:36 -0300

    I knew i should have deleted this!

--- contextReply.js
+++ contextReply.js
@@ -3,7 +3,5 @@ module.exports = (context, value, resp) =>{
         "context": 'https://localhost:3000' + context,
         "value": value,
     }
-    resp.header("Access-Control-Allow-Origin", "*")
-    resp.header("Access-Control-Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept");
     resp.json(reply)
 }
