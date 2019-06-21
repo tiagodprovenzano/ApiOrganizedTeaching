579718b61f97975de78cfd37f4b240c21c29abfc
Tiago Dias <tiagodprovenzano@gmail.com>
2019-06-21 17:01:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./config/custom-express')()
-// app.set('port', (process.env.PORT || 4000));
-// app.listen(app.get('port'), ()=>{
-app.listen(3000, ()=>{
+app.set('port', (process.env.PORT || 4000));
+app.listen(app.get('port'), ()=>{
+// app.listen(3000, ()=>{
     console.log('Starting OrganizedTeaching API');
 })
